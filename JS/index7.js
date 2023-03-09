894fc0bd89854e9de4d6088f23576fd445cac340
stop-zero <jungjy0097@naver.com>
2023-03-09 22:38:06 +0900

    fix an error

--- JS/index7.js
+++ JS/index7.js
@@ -1,17 +1,15 @@
-console.log("연결");
+console.log("연결!");
 
 for (let i = 0; i < 10; i++) {
   console.log("안녕", i);
 }
-console.log("------------------");
+console.log("-----------");
 for (let i = 0; i < 10; i += 2) {
-  console.log(`안녕${i}`);
+  console.log(`안녕 ${i}`);
 }
-
 for (let i = 0; i < 10; i = i + 3) {
   console.log(i);
 }
-
 // 1부터 5까지 출력
 for (let i = 1; i <= 5; i++) {
   console.log(i);
@@ -20,7 +18,6 @@ for (let i = 0; i < 5; i++) {
   console.log(i + 1);
 }
 
-console.log("-------------");
 // 5부터 1까지 출력
 for (let i = 5; i > 0; i--) {
   console.log(i);
@@ -29,10 +26,9 @@ for (let i = 5; i > 0; i--) {
 let n = 11;
 let sum = 0;
 // 1부터 n까지의 합
-
 for (let i = 1; i < n + 1; i++) {
   sum = sum + i;
-  // sum +=i
+  //   sum +=i
 }
 console.log(sum);
 
@@ -42,6 +38,7 @@ for (let i = 0; i < fruits.length; i++) {
   console.log(fruits[i]);
 }
 
+// 배열 내의 숫자 합 구하기
 let numsArr = [90, 50, 30, 20, 11];
 let numsSum = 0;
 for (let i = 0; i < numsArr.length; i++) {
@@ -49,7 +46,7 @@ for (let i = 0; i < numsArr.length; i++) {
 }
 console.log(numsSum);
 
-// 1~20짝수일 때의 합 구하기
+// 1~20 짝수일 때의 합 구하기
 let sum2 = 0;
 for (let i = 0; i <= 20; i++) {
   if (i % 2 === 0) {
@@ -58,54 +55,33 @@ for (let i = 0; i <= 20; i++) {
 }
 console.log(sum2);
 
-//실습 13배수 찾기
-// let num3 = Number(prompt("숫자를 입력해주세요."));
-/* 
-for (let i = 1; i < num3 + 1; i++) {
-  if (i % 13 === 0 && i % 2 === 1) {
-    // 앞에 false인 확률이 높은 애를 써주면 효율적임
-    console.log(i);
-  }
-}
- */
-// it's mine
-// for (let i = 0; i <= 10000; i++) {
+//실습 13 배수이면서 홀수인 수
+// let num3 = Number(prompt('숫자를 입력해주세요'));
+// for (let i = 1; i < num3 + 1; i++) {
 //   if (i % 13 === 0 && i % 2 === 1) {
+// 앞에 false인 확률이 높은 애를 써주면 효율적임
 //     console.log(i);
 //   }
 // }
 
-//실습 구구단
-
-// for (let i = 1; i <= 9; i++) {
-//   console.log("---" + i + "단---");
-//   for (let n = 1; n <= 9; n++) {
-//     n = n * i;
-//     console.log(n + "*" + i + "=" + n * 1);
-//   }
-// }
-
-/* for (let i = 2; i < 10; i++) {
+// 구구단 만들어보기
+for (let i = 2; i < 10; i++) {
   console.log(`${i}단`);
-  for (let j = 1; j < 10; i++) {
-    console.log(`${i}X${j}=${i * j}`);
+  for (let j = 1; j < 10; j++) {
+    console.log(`${i}x${j}=${i * j}`);
   }
 }
- */
-
 /* 2. while */
 let n2 = 1;
 while (n2 <= 5) {
   console.log(n2);
   n2++;
 }
-
 n2 = 10;
-while (n2 >= 4) {
+while (n2 >= 5) {
   console.log(n2);
-  n2++;
+  n2--;
 }
-
 n2 = 10;
 while (n2 >= 1) {
   if (n2 % 2 === 0) {
@@ -113,23 +89,19 @@ while (n2 >= 1) {
   }
   n2--;
 }
-
 n2 = 1;
 while (true) {
   console.log("안녕");
-  if (n2 === 10) {
+  if (n2 == 10) {
     break;
   }
-
   n2++;
 }
-
 n2 = 0;
 while (confirm("계속 진행?")) {
   n2++;
   alert(`${n2}번째 alert 창`);
 }
-
 // continue
 let sum3 = 0;
 for (let i = 0; i < 10; i++) {
@@ -138,5 +110,4 @@ for (let i = 0; i < 10; i++) {
   }
   sum3 += i;
 }
-
 console.log(sum3);
